800660deb4ba66f1b39c918d5983bd0e83a4aa04
dandanthefisherman <dandanthefisherman@hotmail.com>
2022-01-19 10:27:31 +0000

    inital2
    inital2

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Dapr.Client;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using guardi.Data;
+using System.Linq;
+
+namespace guardi.Pages
+{
+    public class IndexModel : PageModel
+    {
+        
+
+         public IndexModel()
+        {
+            //_daprClient = daprClient ?? throw new ArgumentNullException(nameof(daprClient));
+        }
+
+        public async Task OnGet()
+        {
+            //var forecasts = await _daprClient.InvokeMethodAsync<IEnumerable<Article>>(
+              //  HttpMethod.Get,
+                //"daprbackend",
+                //"article");
+
+             //List<Article> ArticleList = InMemoryDatabase.ListOfArticles();
+
+            ViewData["articledata"] = InMemoryDatabase.ListOfArticles();
+
+            List<Article> covidlist = InMemoryDatabase.ListOfArticles();
+
+            ViewData["covidarticles"] = covidlist.Where(lo => lo.ArticleType == "Covid").ToList();
+
+            
+            
+        }
+    }
+}
